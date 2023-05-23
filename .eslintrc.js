8a30797aedca91135ef276d74e5659482a673051
SergeiStepantsov <info@3dpstudio.ru>
2023-05-24 00:59:33 +0600

    fix:autotest #1 1st rewie

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
-module.exports = {
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
-  rules: {
-    "no-underscore-dangle": ["error", { allow: ["_id"] }],
-    quotes: ["error", "double", { avoidEscape: true }],
-    "prefer-destructuring": ["error", { object: false, array: false }],
-  },
+{
+    extends: ["airbnb-base", "plugin:prettier/recommended"],
+    rules: {
+      "no-underscore-dangle": ["error", { allow: ["_id"] }],
+      quotes: ["error", "double", { avoidEscape: true }],
+      "prefer-destructuring": ["error", { object: false, array: false }],
+    },
 };
